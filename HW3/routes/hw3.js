c68ddc93dc50829d05c18d4a7160f7f115843613
dezhouw <hongsinwang@gmail.com>
2019-03-22 13:14:02 -0400

    modify hw3 and add images

--- HW3/routes/hw3.js
+++ HW3/routes/hw3.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+/* retrieve data from external API */
+var Request = require("request");
+
+let URL = "http://api.weatherunlocked.com/api/current/42.36,-71.01?app_id=6fdf098b&app_key=3fbf5b182fe26df5d5eaf744c3311d12";
+
+
+const retrieveData = () => {
+	return new Promise(function (resolve, reject) {
+
+		Request.get(URL, (error, response, body) => {
+			if(error) {
+				resolve(console.dir(error));
+			}
+			resolve(JSON.parse(body));
+		});
+	});
+};
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hw3', { title: 'hw3 test' });
+	retrieveData()
+		.then((value) => {
+			res.render('hw3', {
+				title: 'Boston weather (currrent)',
+				data: value
+			});
+		});
 });
 
 module.exports = router;
